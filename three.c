6bae9dd26449404eee2d8a2f3c5a1de8baab8ad1
lordnamasivayam <50618802+lordnamasivayam@users.noreply.github.com>
2019-05-17 19:07:13 +0530

    Update three.c

--- three.c
+++ three.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
 int a,b,c;
-clrscr();
 printf("Enter the three number\n");
 scanf("%d%d%d",&a,&b,&c);
 if(a>b)
@@ -12,5 +10,4 @@ else if(b>c)
 printf(" b is greater");
 else
 printf(" c is greater");
-getch();
 }
