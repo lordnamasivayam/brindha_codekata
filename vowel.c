7238340700b5e79be7b0e0650151112673cb38a6
lordnamasivayam <50618802+lordnamasivayam@users.noreply.github.com>
2019-05-19 04:59:44 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    int lc_Vowel, uc_Vowel;
+
+    printf("Please Enter an alphabet: \n");
+    scanf(" %c", &ch);
+
+    lc_Vowel = (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    uc_Vowel = (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
+
+    if (lc_Vowel || uc_Vowel) {
+    	printf("\n %c is a VOWEL.", ch);
+	}
+    else {
+    	printf("\n %c is a CONSONANT.", ch);
+	}
+    return 0;
+}
