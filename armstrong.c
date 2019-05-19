2b76c87cfb286b546672fe982c86856d2d270cf8
lordnamasivayam <50618802+lordnamasivayam@users.noreply.github.com>
2019-05-19 18:54:46 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,s=0,a,b;
+    scanf("%d",&n);
+    a=n;
+    while(a!=0)
+    {
+        b=a%10;
+        s=s+b*b*b;
+        a=a/10;
+    }
+    if(n==s)
+    printf("Yes");
+    else
+    printf("no");
+    return 0;
+}
