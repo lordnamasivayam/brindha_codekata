5bb45678defde091e909e9d9844456c9f739fb61
lordnamasivayam <50618802+lordnamasivayam@users.noreply.github.com>
2019-05-24 05:45:26 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int i,j,a[50],t,n,l;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n-1;i++)
+for(j=i+1;j<n;j++)
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+if(n%2==0)
+l=(a[(n+1)/2]+a[n/2])/2;
+else
+l=a[n/2];
+for(i=0;i<n;i++)
+printf("%d ",a[i]);
+printf("\n");
+printf("%d",l);
+return 0;
+}
