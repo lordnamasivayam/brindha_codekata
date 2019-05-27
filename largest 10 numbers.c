c0dc04992f864b532327ddcc7d944a16e681522a
lordnamasivayam <50618802+lordnamasivayam@users.noreply.github.com>
2019-05-27 19:49:25 +0530

    Create largest 10 numbers.c

---
+++ largest 10 numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,a[10],greatest;
+for(i=0;i<10;i++)
+scanf("%d",&a[i]);
+greatest=a[0];
+for(i=0;i<10;i++)
+if(a[i]>greatest)
+{
+greatest=a[i];
+}
+printf("%d ",greatest);
+return 0;
+}
