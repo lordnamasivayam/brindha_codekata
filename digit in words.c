e0cbf37ded9727dd8105fe7a1eb884affe37bf4f
lordnamasivayam <50618802+lordnamasivayam@users.noreply.github.com>
2019-05-30 05:26:59 +0530

    Create digit in words.c

---
+++ digit in words.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+int s=0,n;
+scanf("%d",&n);
+while(n>0)
+{
+s=(s*10)+(n%10);
+n=n/10;
+}
+while(s>0)
+{
+switch(s%10)
+{
+case 0:printf("zero");break;
+case 1:printf("one ");break;
+case 2:printf("two ");break;
+case 3:printf("three ");break;
+case 4:printf("four ");break;
+case 5:printf("five ");break;
+case 6:printf("six ");break;
+case 7:printf("seven ");break;
+case 8:printf("eight ");break;
+case 9:printf("nine ");break;
+}
+s=s/10;
+}
+return 0;
+}
