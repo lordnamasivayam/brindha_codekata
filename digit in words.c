aea2492793870d1dec2cab9f78f706c2e2e80f69
lordnamasivayam <50618802+lordnamasivayam@users.noreply.github.com>
2019-05-30 05:28:53 +0530

    Update digit in words.c

--- digit in words.c
+++ digit in words.c
@@ -12,16 +12,16 @@ while(s>0)
 {
 switch(s%10)
 {
-case 0:printf("zero");break;
-case 1:printf("one ");break;
-case 2:printf("two ");break;
-case 3:printf("three ");break;
-case 4:printf("four ");break;
-case 5:printf("five ");break;
-case 6:printf("six ");break;
-case 7:printf("seven ");break;
-case 8:printf("eight ");break;
-case 9:printf("nine ");break;
+case 0:printf("Zero");break;
+case 1:printf("One ");break;
+case 2:printf("Two ");break;
+case 3:printf("Three ");break;
+case 4:printf("Four ");break;
+case 5:printf("Five ");break;
+case 6:printf("Six ");break;
+case 7:printf("Seven ");break;
+case 8:printf("Eight ");break;
+case 9:printf("Nine ");break;
 }
 s=s/10;
 }
