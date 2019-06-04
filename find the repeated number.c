4980312435b13ebf9853146e5243aaefd25ccb1f
lordnamasivayam <50618802+lordnamasivayam@users.noreply.github.com>
2019-06-04 18:34:43 +0530

    Create find the repeated number.c

---
+++ find the repeated number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,k,i,a[10],f;
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+{
+if(a[i]==k)
+f==1;
+}
+printf("%d",k);
+return 0;
+}
