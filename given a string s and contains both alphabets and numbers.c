a689e9dc3c9e752c69d5cfec6ebb64c68916990d
lordnamasivayam <50618802+lordnamasivayam@users.noreply.github.com>
2019-06-09 20:15:59 +0530

    Create given a string s and contains both alphabets and numbers.c

---
+++ given a string s and contains both alphabets and numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int f,i;
+char st[20];
+gets(st);
+for(i=0;st[i]!='\0';i++)
+{
+if((st[i]>='a' || st[i]<='z' || st[i]>='A' || st[i]<='Z') && (st[i]>='0' && st[i]<='9'))
+f=1;
+}
+if(f==1)
+printf("yes");
+else
+printf("no");
+return 0;
+}
